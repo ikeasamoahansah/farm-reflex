dff794f78c3a1ee11091ec27d256aaa5c1adc872
Ike <dylanraspino264@gmail.com>
2024-05-22 21:09:51 +0000

    fresh template

--- farm_reflex/farm_reflex.py
+++ farm_reflex/farm_reflex.py
@@ -1,53 +1,14 @@
+"""Welcome to Reflex!."""
+
+# Import all the pages.
+from farm_reflex.pages import *
+
 import reflex as rx
 
-from . import style
-from .state import State
+
+class State(rx.State):
+    """Define empty state to allow access to rx.State.router."""
 
 
-def qa(question: str, answer: str) -> rx.Component:
-    return rx.box(
-        rx.box(
-            rx.text(question, style=style.question_style),
-            text_align="right",
-        ),
-        rx.box(
-            rx.text(answer, style=style.answer_style),
-            text_align="left",
-        ),
-        margin_y="2em",
-    )
-
-
-def chat() -> rx.Component:
-    return rx.box(
-        rx.foreach(
-            State.chat_history,
-            lambda messages: qa(messages[0], messages[1])
-        ),
-    )
-
-
-def action_bar() -> rx.Component:
-    return rx.hstack(
-        rx.input(
-            value=State.question,
-            placeholder="Ask a question",
-            on_change=State.set_question,
-            style=style.input_style,
-        ),
-        rx.button("Ask", on_click=State.answer, style=style.button_style),
-    )
-
-
-def index() -> rx.Component:
-    return rx.center(
-        rx.vstack(
-            chat(),
-            action_bar(),
-            align="center",
-        )
-    )
-
-
+# Create the app.
 app = rx.App()
-app.add_page(index)
